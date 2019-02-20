263af8762274476c7d313b62bae35dca67053e39
Olivia Mellen <omellen2269@jh-mba158-18.local>
2019-02-20 15:10:38 -0600

    added cells

--- tableView/EmojiTableTableViewController.swift
+++ tableView/EmojiTableTableViewController.swift
@@ -23,29 +23,33 @@ class EmojiTableTableViewController: UITableViewController {
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        tableView.dataSource = self
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
+    
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        if section == 0 {
+           return emojis.count
+        } else {
+            return 0
+        }
+       
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
 
-        // Configure the cell...
-
+        let emoji = emojis[indexPath.row]
+        cell.textLabel?.text = "\(emoji.symbol) - \(emoji.name)"
+        cell.detailTextLabel?.text = "\(emojis[indexPath.row].description)"
+        
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
